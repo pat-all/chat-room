855f1419191e7bedfb6a812e03cbf1acdf4b51e5
pat-all <topalov.petr@gmail.com>
2020-02-20 11:32:15 +0200

    web-app build

---
+++ web-client/build/precache-manifest.fcf1c4cdcd98450fe0fb6b2ea4363910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c61f54ad72e7576d008cf88d7a1ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94d529015152d4acb8b5",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "80a7feb4b710351f6459",
+    "url": "/static/css/main.db4be8fb.chunk.css"
+  },
+  {
+    "revision": "94d529015152d4acb8b5",
+    "url": "/static/js/2.b5c32889.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "/static/js/2.b5c32889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a7feb4b710351f6459",
+    "url": "/static/js/main.d7cb09ad.chunk.js"
+  },
+  {
+    "revision": "7c7709ffdcf12ffcfa64",
+    "url": "/static/js/runtime-main.62ca1b0d.js"
+  }
+]);
